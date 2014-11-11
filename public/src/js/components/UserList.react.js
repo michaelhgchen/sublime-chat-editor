23b419c40f4515f5a9308b3ec2e71bb38b989c6c
Michael Hon-Gen Chen <michaelhgchen@gmail.com>
2014-11-11 15:59:17 -0800

    Add user list folder toggling

--- public/src/js/components/UserList.react.js
+++ public/src/js/components/UserList.react.js
@@ -1,28 +1,53 @@
 var
-  React = require('react/addons'),
+  React              = require('react/addons'),
+  CSSTransitionGroup = require('react/addons').addons.CSSTransitionGroup,
   UserList;
 
 UserList = React.createClass({
-  render: function() {
-    var allUsers;
+  getInitialState: function() {
+    return {
+      open: true
+    }
+  },
 
-    allUsers = Object.keys(this.props.allUsers).map(function(user) {
-      return (
-        <li key={user}>
-          <i className="icon-file-code-o"></i>
-          {user}.js
-        </li>
-      );
+  toggleContents: function() {
+    this.setState({
+      open: !this.state.open
     });
+  },
+
+  render: function() {
+    var folderContents, allUsers;
+
+    if(this.state.open) {
+      allUsers = Object.keys(this.props.allUsers).map(function(user) {
+        return (
+          <li key={user}>
+            <i className="icon-file-code-o"></i>
+            {user}.js
+          </li>
+        );
+      });
+
+      folderContents = (
+        <ul key="open-folder" className="folder-contents">
+          {allUsers}
+        </ul>
+      );
+    } else {
+      folderContents = <ul key="closed-folder"></ul>;
+    }
 
     return (
       <div className="user-list-container no-mobile">
         <div className="user-list-title">Folders</div>
         <ul className="folder">
-          <li className="open"><i className="icon-folder-open-o"></i>users</li>
-          <ul className="folder-contents">
-            {allUsers}
-          </ul>
+          <li onClick={this.toggleContents} className={this.state.open ? 'open' : 'closed'}>
+            <i className="icon-arrow"></i><i className="icon-folder">
+            </i>users</li>
+          <CSSTransitionGroup transitionName="folder-toggle-transition">
+            {folderContents}
+          </CSSTransitionGroup>
         </ul>
       </div>
     );
